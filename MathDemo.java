3b766d707aa2b0232fce7659cd3523de8d22fb24
Yusuf.S <37862006+yusuf00s114@users.noreply.github.com>
2026-02-24 10:43:38 +0400

    Add files via upload

---
+++ MathDemo.java
@@ -0,0 +1,30 @@
+public class MathDemo {
+
+    int min(int a, int b){
+        return a < b ? a : b;
+    }
+
+    int max(int a, int b){
+        return a > b ? a : b;
+    }
+
+    int sum(int args[]){
+        int result = 0;
+        for (int i = 0; i < args.length; i++) {
+            result += args[i];
+        }
+        return result;
+    }
+
+    /*
+    Answer for f:
+        The advantage of defining these methods as static would be that
+        you wouldn't need to instantiate the class in order to call the methods.
+        This would be useful, for example, when creating helper methods, such as
+        ones in the Mathf class.
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
